7192ea864feba2e4c4b7e8e6ae64af7b903cdee0
andrewosmondcode <46552254+andrewosmondcode@users.noreply.github.com>
2020-03-29 11:48:46 +0700

    Sticky navbar, scroll to top animation, testing responsive

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
+
+$(document).ready(function(){
+    //Set sticky to navbar when stop scrolling
+    $(window).scroll(function(){
+    	$('.navbar').removeClass('sticky');
+    	clearTimeout($.data(this, "scrollCheck"));
+    	$.data(this, "scrollCheck", setTimeout(function (){
+    		$('.navbar').addClass('sticky');
+    	}, 250));
+    });
+});
