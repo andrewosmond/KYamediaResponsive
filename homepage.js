d10c6ba7f6d932bc410731df8a96a45f163ccaf1
andrewosmondcode <46552254+andrewosmondcode@users.noreply.github.com>
2020-04-01 20:55:38 +0700

    Homepage done

--- homepage.js
+++ homepage.js
@@ -1,3 +1,7 @@
+var slideIndex = 1;
+var timer = null;
+showSlides(slideIndex);
+
 $(document).ready(function(){
     //Check to see if the window is top if not then display button
     $(window).scroll(function(){
@@ -15,3 +19,49 @@ $(document).ready(function(){
     });
 
 });
+
+function currentSlide(n){
+    clearTimeout(timer);
+    showSlides(slideIndex = n)
+}
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    if (n == undefined) {++slideIndex}
+    if (n > slides.length || slideIndex > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        tabcontent[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "");   
+    }
+    slides[slideIndex-1].style.display = "block";
+    tabcontent[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    timer = setTimeout(showSlides, 4000);
+}
+
+function prevBooks(){
+    var i;
+    var books = document.getElementsByClassName("sliders");
+    var tail = books[books.length - 1].innerHTML;
+    for (i = books.length-1; i > 0; i--){
+        books[i].innerHTML = books[i-1].innerHTML;
+    }
+    books[0].innerHTML = tail;
+}
+
+function nextBooks() {
+    var i;
+    var books = document.getElementsByClassName("sliders");
+    var head = books[0].innerHTML;
+    for (i = 1; i < books.length; i++){
+        books[i-1].innerHTML = books[i].innerHTML;
+    }
+    books[books.length - 1].innerHTML = head;
+}
